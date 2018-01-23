08edeffd7f419f0ea2fafe4e7143990ee1219c9a
mstarner <mstarner8@gmail.com>
2018-01-23 08:17:22 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+        System.out.println("tell me your input");
+        int number = input.nextInt();
+int i;
+        for (i = 1; i <= number; i++)
+        {
+            int start = 1;
+            start = 
+        }
+    }
+}
